85d891b7fb1d4157eb1dd2fc067f8590070a1c09
camisetadebby <camisetadebby@gmail.com>
2025-03-05 22:16:55 +0000

    Add 1.rs

---
+++ src/1.rs
@@ -0,0 +1,9 @@
+use std::collections::HashMap;
+
+fn main() {
+    let mut map = HashMap::new();
+    map.insert("apple", 3);
+    map.insert("banana", 4);
+    map.insert("orange", 5);
+    println!("{:?}", map);
+}
