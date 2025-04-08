a3cb8ed656023d587d95e24413b84d8efd3c2cce
camisetadebby <camisetadebby@gmail.com>
2025-04-08 13:32:38 +0000

    Add 27.rs

---
+++ src/27.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let first_num: i32 = 5;
+    let second_num: i32 = 10;
+
+    if first_num > second_num {
+        println!("The first number is greater than the second number.");
+    } else {
+        println!("The first number is not greater than the second number.");
+    }
+}
